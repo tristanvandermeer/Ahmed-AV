f9e0d73b7929fc21da56efb25f9cf1c36d03e233
RealTribn <tristan.vandermeer@gmail.com>
2025-06-12 10:30:53 +0100

    app api setup + corners fix

--- Ask Ahmed/src/main.js
+++ Ask Ahmed/src/main.js
@@ -1,5 +1,19 @@
 import "xp.css/dist/XP.css"
 import './styles.css';
 
-document.getElementById('clickMeBtn').disabled = false;
+const { invoke } = window.tauri.core;
+const { listen } = window.tauri.event;
+const { getCurrent } = window.tauri.window;
+
+
+
+setInterval(() => {
+    const now = new Date();
+    document.getElementById("text").innerText = `The time is: ${now.toLocaleTimeString()}`;
+  }, 1000); // 1000ms = 1 second
+
+document.getElementById("ok").addEventListener("click", () => {
+  document.getElementById("text").innerText = `stop it`;
+});
+
 console.log('Frontend loaded');
